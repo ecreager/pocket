d76e60e27ccda45f3168cd6cb7327d3d49729e94
ecreager <elliot.creager@gmail.com>
2016-12-26 23:31:45 -0500

    remove irrelevant code

--- gray_table.py
+++ gray_table.py
@@ -4,17 +4,22 @@ import os
 
 # write a random max msp table
 
-filename = 'gray_table.txt'
-string = 'table '
-n = 7
-m = 7
-code = gray_code.single_1d_gray_code(n=n, m=m, do_print=True)
-for value in code.values():
-	string += '%i ' % value
+def write_gray_table():
 
-if os.path.exists(filename):
-	os.remove(filename)
+	filename = 'gray_table.txt'
+	string = 'table '
+	n = 7
+	m = 7
+	code = gray_code.single_1d_gray_code(n=n, m=m, do_print=True)
+	for value in code.values():
+		string += '%i ' % value
 
-with open(filename, "w") as f:
-    f.write(string)
-print('wrote table to disk')
+	if os.path.exists(filename):
+		os.remove(filename)
+
+	with open(filename, "w") as f:
+	    f.write(string)
+	print('wrote table to disk')
+
+if __name__ == '__main__':
+	write_gray_table()
